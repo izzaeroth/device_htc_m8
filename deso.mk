fd096624a371d61e523f13a4b14c5cd69341f2ac
remicks <lost.junglist@gmail.com>
2015-02-13 12:06:42 -0600

    Desolation complete
    We are Desolation

--- deso.mk
+++ deso.mk
@@ -3,7 +3,8 @@ $(call inherit-product, device/htc/m8/full_m8.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/deso/config/nfc_enhanced.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/deso/config/common_full_phone.mk)
+# Inherit some common Desolation stuff.
+$(call inherit-product, vendor/deso/config/common.mk)
+$(call inherit-product, vendor/deso/config/telephony.mk)
 
 PRODUCT_NAME := deso_m8
