442253b2ac18e6c77ff43bd82512909729b6aa88
buckmarble <forrestmarble@gmail.com>
2015-03-02 18:36:46 -0500

    enable telepony.mk for apns

--- deso.mk
+++ deso.mk
@@ -5,6 +5,6 @@ $(call inherit-product, vendor/deso/config/nfc_enhanced.mk)
 
 # Inherit some common Desolation stuff.
 $(call inherit-product, vendor/deso/config/common.mk)
-#$(call inherit-product, vendor/deso/config/telephony.mk)
+$(call inherit-product, vendor/deso/config/telephony.mk)
 
 PRODUCT_NAME := deso_m8
