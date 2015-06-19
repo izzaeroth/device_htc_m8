4d510d6ba7fc2d055a16cf20a8d271889f038c7a
izzaeroth <izzaeroth@gmail.com>
2015-06-19 07:05:22 -0500

    Update deso.mk

--- deso.mk
+++ deso.mk
@@ -5,5 +5,6 @@ $(call inherit-product, vendor/deso/config/nfc_enhanced.mk)
 
 # Inherit some common Desolation stuff.
 $(call inherit-product, vendor/deso/config/common_phone.mk)
+$(call inherit-product, vendor/deso/config/common.mk)
 
 PRODUCT_NAME := deso_m8
