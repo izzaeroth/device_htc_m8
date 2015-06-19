b18dee34803bbf0c2a87863c6c9a1d579c21ad6b
izzaeroth <izzaeroth@gmail.com>
2015-06-18 21:05:13 -0500

    Common -> Common Phone

--- deso.mk
+++ deso.mk
@@ -4,7 +4,6 @@ $(call inherit-product, device/htc/m8/full_m8.mk)
 $(call inherit-product, vendor/deso/config/nfc_enhanced.mk)
 
 # Inherit some common Desolation stuff.
-$(call inherit-product, vendor/deso/config/common.mk)
-$(call inherit-product, vendor/deso/config/telephony.mk)
+$(call inherit-product, vendor/deso/config/common_phone.mk)
 
 PRODUCT_NAME := deso_m8
