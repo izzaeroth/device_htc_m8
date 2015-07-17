c5ccee4057d2d683caa2d1683434cc3964fc8f4c
izzaeroth <izzaeroth@gmail.com>
2015-07-17 16:11:55 -0500

    updating device hardware info

--- deso.mk
+++ deso.mk
@@ -8,3 +8,12 @@ $(call inherit-product, vendor/deso/config/common_phone.mk)
 $(call inherit-product, vendor/deso/config/common.mk)
 
 PRODUCT_NAME := deso_m8
+
+# Device hardware info
+PRODUCT_CHIPSET := Qualcomm MSM8974AB Snapdragon 801
+PRODUCT_CPU := Quad-core 2.3 GHz Krait 400
+PRODUCT_GPU := Adreno 330
+PRODUCT_REAR_CAM := Dual 4.0 MP, 2688х1520 pixels
+PRODUCT_FRONT_CAM := 5.0 MP
+PRODUCT_SCREEN_SIZE := 5.0 inch ~441 ppi, Super LCD3
+PRODUCT_SCREEN_RES := 1080 x 1920 pixels
